4bbe9daa7fbfd4b0717eeaee765d9ad6038944e7
Jonathan Hidalgo Mora <jon_hid@hotmail.com>
2022-07-06 09:17:35 +0200

    test new functionality of the getCocktails function at cocktail service

--- test/src/services/cocktail.test.js
+++ test/src/services/cocktail.test.js
@@ -5,12 +5,24 @@ import { cocktailsMock } from '../../__mocks__/cocktails';
 jest.mock('axios');
 
 describe('Testing cocktail service...', () => {
-  it('Testing getCocktails function...', async () => {
+  it('Testing getCocktails function without params...', async () => {
     axios.get.mockResolvedValueOnce(cocktailsMock);
     const cocktails = await getCocktails();
     expect(axios.get).toBeCalledTimes(1);
     expect(axios.get).toHaveBeenCalledWith(endpoint);
     expect(cocktailsMock.data.drinks[0].idDrink).toEqual(cocktails[0].idDrink);
     expect(cocktailsMock.data.drinks.length).toEqual(cocktails.length);
+  })
+
+  it('Testing getCocktails function with params...', async () => {
+    axios.get.mockResolvedValueOnce(cocktailsMock);
+    await getCocktails('ab');
+    expect(axios.get).toHaveBeenCalledWith(`${endpoint}ab`);
+  })
+
+  it('Testing getCocktails function without cocktails at response...', async () => {
+    axios.get.mockResolvedValueOnce({});
+    const cocktails = await getCocktails();
+    expect(cocktails.length).toEqual(0);
   })
 })
