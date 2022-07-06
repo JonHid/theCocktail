b7e2ca764e640a2f12380abb5bcc48a17cbb40cc
Jonathan Hidalgo Mora <jon_hid@hotmail.com>
2022-07-06 09:14:59 +0200

    improved getCocktails function receiving cocktail name param

--- src/services/cocktail.ts
+++ src/services/cocktail.ts
@@ -3,9 +3,10 @@ import { ICocktail } from '../interfaces/cocktail';
 
 const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-async function getCocktails(): Promise<Array<ICocktail>> {
-  const response = await axios.get(endpoint);
-  return response.data?.drinks;
+async function getCocktails(cocktailName?: string): Promise<Array<ICocktail>> {
+  const url = cocktailName ? endpoint + cocktailName : endpoint;
+  const response = await axios.get(url);
+  return response.data?.drinks || [];
 }
 
 export {
