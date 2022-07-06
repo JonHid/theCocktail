05e44159125811018cf253b6330d0df484b61051
Jonathan Hidalgo Mora <jon_hid@hotmail.com>
2022-07-06 18:46:54 +0200

    add like button to cocktail component

--- src/components/cocktail/Cocktail.tsx
+++ src/components/cocktail/Cocktail.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ICocktail } from '../../interfaces/cocktail';
 import './Cocktail.css';
 
 const Cocktail: React.FunctionComponent<ICocktail> = (props: ICocktail) => {
   const { strDrink, strDrinkThumb, strInstructions } = props;
+  const [ like, setLike ] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLike(false);
+  }, [strDrink]);
+
+  function clickHandler(e: React.MouseEvent<HTMLElement>) {
+    setLike(!like);
+  }
 
   return (
     <div className='cocktailContainer'>
-      <div className='cocktailHeader'>{ strDrink }</div>
+      <div className='cocktailHeader'>
+        { strDrink }
+        <div className={`iconContainer ${ like ? 'like' : '' }`} onClick={ clickHandler }>
+          <i className='material-icons'>favorite</i>
+        </div>
+      </div>
       <div className='cocktailBody'>
         <img className='cocktailImg' src={ strDrinkThumb } alt={ `${ strDrink }.img` } />
         <div className='cocktailDescription' >{ strInstructions }</div>
