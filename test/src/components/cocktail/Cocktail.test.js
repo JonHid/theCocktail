444196a156f8a1f071bb819f65a486998a9de82a
Jonathan Hidalgo Mora <jon_hid@hotmail.com>
2022-07-06 18:47:21 +0200

    add tests of the like button

--- test/src/components/cocktail/Cocktail.test.js
+++ test/src/components/cocktail/Cocktail.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Cocktail from '../../../../src/components/cocktail/Cocktail';
 import { cocktailsMock } from '../../../__mocks__/cocktails';
+
+function getIconContainer(renderedCocktailComponent) {
+  return renderedCocktailComponent.queryByText((content, element) => {
+    return element.classList.contains('iconContainer');
+  });
+}
 
 describe('Testing Cocktail component...', () => {
   it('Rendering component...', () => {
@@ -19,5 +25,27 @@ describe('Testing Cocktail component...', () => {
     const cocktailImg = renderedCocktailComponent.getByAltText(`${cocktail.strDrink}.img`);
     expect(cocktailImg.src).toBe(cocktail.strDrinkThumb);
     expect(renderedCocktailComponent.container).toHaveTextContent(cocktail.strInstructions);
+  })
+
+  it('Toogle like button...', () => {
+    const cocktail = cocktailsMock.data.drinks[0];
+    const cocktailComponent =
+    <Cocktail
+      strDrinkThumb={ cocktail.strDrinkThumb }
+      strDrink={ cocktail.strDrink }
+      strInstructions={ cocktail.strInstructions }
+    />
+
+    const renderedCocktailComponent = render(cocktailComponent);
+
+    const iconContainerElement = getIconContainer(renderedCocktailComponent);
+    fireEvent.click(iconContainerElement);
+    const likeElement = getIconContainer(renderedCocktailComponent);
+    expect(likeElement.classList.contains('iconContainer')).toBe(true);
+    expect(likeElement.classList.contains('like')).toBe(true);
+
+    fireEvent.click(likeElement);
+    const notLikeElement = getIconContainer(renderedCocktailComponent);
+    expect(notLikeElement.classList.contains('like')).toBe(false);
   })
 })
