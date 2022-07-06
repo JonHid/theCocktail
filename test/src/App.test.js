ac726be6103fc193bd8c5fdfffe1467cbbfbc39f
Jonathan Hidalgo Mora <jon_hid@hotmail.com>
2022-07-06 09:15:46 +0200

    add MockFinder component to App component test

--- test/src/App.test.js
+++ test/src/App.test.js
@@ -2,14 +2,21 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import App from '../../src/App';
+
 jest.mock('../../src/components/list/List', () => () => {
   const MockList = 'mock-list';
   return <MockList />;
 });
 
+jest.mock('../../src/components/finder/Finder', () => () => {
+  const MockFinder = 'mock-finder';
+  return <MockFinder />;
+});
+
 describe('Testing App component...', () => {
   it('Rendering component...', () => {
     const renderedAppComponent = render(<App/>);
+    expect(renderedAppComponent.container).toContainHTML('<mock-finder />');
     expect(renderedAppComponent.container).toContainHTML('<mock-list />');
   })
 })
