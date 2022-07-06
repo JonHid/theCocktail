49abfde6c54e2b39f8c6af09d884aef05476182b
Jonathan Hidalgo Mora <jon_hid@hotmail.com>
2022-07-06 04:56:58 +0200

    replace fetch by axios

--- src/services/cocktail.ts
+++ src/services/cocktail.ts
@@ -1,11 +1,11 @@
+import axios from 'axios';
 import { ICocktail } from '../interfaces/cocktail';
 
 const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 async function getCocktails(): Promise<Array<ICocktail>> {
-  const response = await fetch(endpoint);
-  const cocktailsResponse = await response.json();
-  return cocktailsResponse.drinks;
+  const response = await axios.get(endpoint);
+  return response.data?.drinks;
 }
 
 export {
